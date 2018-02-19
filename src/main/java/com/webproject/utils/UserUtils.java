a41b29c34bec46b096f0f52f54f35a443f5467b3
ankurupadhyay123 <ankurupadhyay123@hotmail.com>
2018-02-19 21:30:52 +0530

    Created the frontend domain model & translation layer for the basic account

--- src/main/java/com/webproject/utils/UserUtils.java
+++ src/main/java/com/webproject/utils/UserUtils.java
@@ -2,6 +2,7 @@ package com.webproject.utils;
 
 import com.webproject.backend.persistence.domain.backend.User;
 import com.webproject.web.controllers.ForgotMyPasswordController;
+import com.webproject.web.domain.frontend.BasicAccountPayload;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -49,5 +50,20 @@ public class UserUtils {
                         "&token=" +
                         token;
         return passwordResetUrl;
+    }
+
+    public static <T extends BasicAccountPayload> User fromWebUserToDomainUser(T frontendPayload) {
+        User user = new User();
+        user.setUsername(frontendPayload.getUsername());
+        user.setPassword(frontendPayload.getPassword());
+        user.setFirstName(frontendPayload.getFirstName());
+        user.setLastName(frontendPayload.getLastName());
+        user.setEmail(frontendPayload.getEmail());
+        user.setPhoneNumber(frontendPayload.getPhoneNumber());
+        user.setCountry(frontendPayload.getCountry());
+        user.setEnabled(true);
+        user.setDescription(frontendPayload.getDescription());
+
+        return user;
     }
 }
